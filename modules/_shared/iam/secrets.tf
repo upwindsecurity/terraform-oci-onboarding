d83534f897de0ab7324f5cded29f2ab7ccfff375
ahumph <adam.humphreys@upwind.io>
2025-11-25 15:36:41 +0000

    fix(AG-3868): re-enable vaults

--- modules/_shared/iam/secrets.tf
+++ modules/_shared/iam/secrets.tf
@@ -1,87 +1,87 @@
 ### Vault and Vault Key Resources
 
 # Create the Vault for storing secrets
-# resource "oci_kms_vault" "upwind_vault" {
-#   compartment_id   = local.compartment_id
-#   display_name     = format("upwind-vault-%s", local.resource_suffix_hyphen)
-#   # Required for software-only keys
-#   vault_type       = "VIRTUAL_PRIVATE"
-# }
+resource "oci_kms_vault" "upwind_vault" {
+  compartment_id = local.compartment_id
+  display_name   = format("upwind-vault-%s", local.resource_suffix_hyphen)
+  # Required for software-only keys
+  vault_type = "VIRTUAL_PRIVATE"
+}
 
-# # Create the Vault Key for encryption
-# resource "oci_kms_key" "upwind_vault_key" {
-#   compartment_id      = local.compartment_id
-#   display_name        = format("upwind-vault-key-%s", local.resource_suffix_hyphen)
-#   management_endpoint = oci_kms_vault.upwind_vault.management_endpoint
-#   key_shape {
-#     algorithm = "AES"
-#     length    = 32
-#   }
-#   # Required for software-only keys
-#   protection_mode = "SOFTWARE"
-# }
+# Create the Vault Key for encryption
+resource "oci_kms_key" "upwind_vault_key" {
+  compartment_id      = local.compartment_id
+  display_name        = format("upwind-vault-key-%s", local.resource_suffix_hyphen)
+  management_endpoint = oci_kms_vault.upwind_vault.management_endpoint
+  key_shape {
+    algorithm = "AES"
+    length    = 32
+  }
+  # Required for software-only keys
+  protection_mode = "SOFTWARE"
+}
 
 ### Secrets used by Cloudscanners
 
-# resource "oci_vault_secret" "upwind_client_id" {
-#   compartment_id = local.compartment_id
-#   vault_id       = oci_kms_vault.upwind_vault.id
-#   key_id         = oci_kms_key.upwind_vault_key.id
-#   secret_name    = "upwind-client-id-${local.resource_suffix_hyphen}"
-#   secret_content {
-#     content = base64encode(var.upwind_client_id)
-#     content_type = "BASE64"
-#   }
-#   description    = "Upwind client ID for authentication"
-# }
+resource "oci_vault_secret" "upwind_client_id" {
+  compartment_id = local.compartment_id
+  vault_id       = oci_kms_vault.upwind_vault.id
+  key_id         = oci_kms_key.upwind_vault_key.id
+  secret_name    = "upwind-client-id-${local.resource_suffix_hyphen}"
+  secret_content {
+    content      = base64encode(var.upwind_client_id)
+    content_type = "BASE64"
+  }
+  description = "Upwind client ID for authentication"
+}
 
-# resource "oci_vault_secret" "upwind_client_secret" {
-#   compartment_id = local.compartment_id
-#   vault_id       = oci_kms_vault.upwind_vault.id
-#   key_id         = oci_kms_key.upwind_vault_key.id
-#   secret_name    = "upwind-client-secret-${local.resource_suffix_hyphen}"
-#   secret_content {
-#     content = base64encode(var.upwind_client_secret)
-#     content_type = "BASE64"
-#   }
-#   description    = "Upwind client secret for authentication"
-# }
+resource "oci_vault_secret" "upwind_client_secret" {
+  compartment_id = local.compartment_id
+  vault_id       = oci_kms_vault.upwind_vault.id
+  key_id         = oci_kms_key.upwind_vault_key.id
+  secret_name    = "upwind-client-secret-${local.resource_suffix_hyphen}"
+  secret_content {
+    content      = base64encode(var.upwind_client_secret)
+    content_type = "BASE64"
+  }
+  description = "Upwind client secret for authentication"
+}
 
-# resource "oci_vault_secret" "scanner_client_id" {
-#   count          = var.enable_cloudscanners ? 1 : 0
-#   compartment_id = local.compartment_id
-#   vault_id       = oci_kms_vault.upwind_vault.id
-#   key_id         = oci_kms_key.upwind_vault_key.id
-#   secret_name    = "upwind-scanner-client-id-${local.resource_suffix_hyphen}"
-#   secret_content {
-#     content = base64encode(var.scanner_client_id)
-#     content_type = "BASE64"
-#   }
-#   description    = "Upwind scanner client ID for authentication"
-# }
+resource "oci_vault_secret" "scanner_client_id" {
+  count          = var.enable_cloudscanners ? 1 : 0
+  compartment_id = local.compartment_id
+  vault_id       = oci_kms_vault.upwind_vault.id
+  key_id         = oci_kms_key.upwind_vault_key.id
+  secret_name    = "upwind-scanner-client-id-${local.resource_suffix_hyphen}"
+  secret_content {
+    content      = base64encode(var.scanner_client_id)
+    content_type = "BASE64"
+  }
+  description = "Upwind scanner client ID for authentication"
+}
 
-# resource "oci_vault_secret" "scanner_client_secret" {
-#   count          = var.enable_cloudscanners ? 1 : 0
-#   compartment_id = local.compartment_id
-#   vault_id       = oci_kms_vault.upwind_vault.id
-#   key_id         = oci_kms_key.upwind_vault_key.id
-#   secret_name    = "upwind-scanner-client-secret-${local.resource_suffix_hyphen}"
-#   secret_content {
-#     content = base64encode(var.scanner_client_secret)
-#     content_type = "BASE64"
-#   }
-#   description    = "Upwind scanner client secret for authentication"
-# }
+resource "oci_vault_secret" "scanner_client_secret" {
+  count          = var.enable_cloudscanners ? 1 : 0
+  compartment_id = local.compartment_id
+  vault_id       = oci_kms_vault.upwind_vault.id
+  key_id         = oci_kms_key.upwind_vault_key.id
+  secret_name    = "upwind-scanner-client-secret-${local.resource_suffix_hyphen}"
+  secret_content {
+    content      = base64encode(var.scanner_client_secret)
+    content_type = "BASE64"
+  }
+  description = "Upwind scanner client secret for authentication"
+}
 
-# # Create an empty secret but apply the user-defined tags to the resource
-# resource "oci_vault_secret" "terraform_tags" {
-#   compartment_id = local.compartment_id
-#   vault_id       = oci_kms_vault.upwind_vault.id
-#   key_id         = oci_kms_key.upwind_vault_key.id
-#   secret_name    = "upwind-default-tags-${local.resource_suffix_hyphen}"
-#   secret_content {
-#     content = base64encode("tags-stored-as-resource-metadata")
-#     content_type = "BASE64"
-#   }
-#   description    = "Default tags stored as resource metadata"
-# }
+# Create an empty secret but apply the user-defined tags to the resource
+resource "oci_vault_secret" "terraform_tags" {
+  compartment_id = local.compartment_id
+  vault_id       = oci_kms_vault.upwind_vault.id
+  key_id         = oci_kms_key.upwind_vault_key.id
+  secret_name    = "upwind-default-tags-${local.resource_suffix_hyphen}"
+  secret_content {
+    content      = base64encode("tags-stored-as-resource-metadata")
+    content_type = "BASE64"
+  }
+  description = "Default tags stored as resource metadata"
+}
