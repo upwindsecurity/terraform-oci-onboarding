4618c88f93bca3a13be5db6c56371055e27ddc07
Shashank Saxena <shashank.saxena@upwind.io>
2026-05-12 11:09:21 +0100

    fix(AG-8744): added block execution validation for id and secret fields (#46)
    * feat(AG-8744): bumped the minimum required version for oci provider from 7.0.0 to 8.0.0
    
    * feat(AG-8744): added validation where enable_cloudscanners is true, both scanner_client_id and scanner_client_secret must be provided
    
    * feat(AG-8744): bumped terraform version from 1.0 to 1.5
    
    * fix(AG-8744): added block execution validation for id and secret fields
    
    * feat(AG-8744): Added validation and bumped minimum terraform version to 1.5 (#44)
    
    * feat(AG-8744): bumped the minimum required version for oci provider from 7.0.0 to 8.0.0
    
    * feat(AG-8744): added validation where enable_cloudscanners is true, both scanner_client_id and scanner_client_secret must be provided
    
    * feat(AG-8744): bumped terraform version from 1.0 to 1.5
    
    * fix(AG-8744): upgraded tf version to 1.11 from 1.5 and validations

--- validation.tf
+++ validation.tf
@@ -1,6 +1,13 @@
-check "cloudscanner_credentials" {
+check "cloudscanner_credentials_required" {
   assert {
     condition     = !var.enable_cloudscanners || (var.scanner_client_id != "" && var.scanner_client_secret != "")
     error_message = "When enable_cloudscanners is true, both scanner_client_id and scanner_client_secret must be provided."
+  }
+}
+
+check "cloudscanner_credentials_not_allowed" {
+  assert {
+    condition     = var.enable_cloudscanners || (var.scanner_client_id == "" && var.scanner_client_secret == "")
+    error_message = "When enable_cloudscanners is false, scanner_client_id and scanner_client_secret must be empty."
   }
 }
